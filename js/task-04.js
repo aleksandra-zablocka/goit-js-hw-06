abeda3677f5abea0bacefe04e4c4df276bea0622
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-04-15 19:33:20 +0200

    started task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,22 @@
 "use strict";
+// let counterValue = 0;
+let counterValue = document.querySelector("#value");
+
+const decrementClick = () => {
+  counterValue -= 1;
+};
+
+const incrementClick = () => {
+  counterValue += 1;
+};
+
+const buttonDec = document.querySelector('button[data-action="decrement"]');
+console.log(buttonDec);
+
+const buttonInc = document.querySelector('button[data-action="increment"]');
+console.log(buttonInc);
+
+buttonInc.addEventListener("click", incrementClick);
+
+console.log(counterValue);
+console.log(counterValue.textContent);
