430807f9488344da67490713c5c8a11efe830e8f
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-04-17 19:03:11 +0200

    Task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,7 +1,35 @@
 "use strict";
 
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const collection = document.querySelector("#boxes");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes() {
+  let number = input.value;
+  destroyBoxes();
+
+  for (let i = 0; i < number; i += 1) {
+    let size = 30 + i * 10;
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = "5px";
+    collection.append(box);
+  }
+}
+
+function destroyBoxes() {
+  collection.innerHTML = "";
+  input.value = "";
+}
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
