396e0c92aa6469f30154b57bf27fd1204ea3b4e5
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-04-20 15:36:17 +0200

    Corrected task 2

--- js/task-02.js
+++ js/task-02.js
@@ -3,12 +3,24 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 const list = document.querySelector("#ingredients");
 
+// Metoda 1
+// ingredients.forEach((ingredient) => {
+//   const li = document.createElement("li");
+//   li.classList.add("item");
+//   li.textContent = ingredient;
+//   list.append(li);
+// });
+// console.log(list);
+
+// Metoda 2
+let array = [];
+
 ingredients.forEach((ingredient) => {
   const li = document.createElement("li");
   li.classList.add("item");
   li.textContent = ingredient;
-  // list.append(li);
+  array.push(li);
 });
 
-list.append();
+list.append(...array);
 console.log(list);
